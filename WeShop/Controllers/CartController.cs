1c2b2f7c8509e1a819ce02bfae1395315f864cf7
张浩 <@DESKTOP-PVLU58K>
2017-11-07 11:31:55 +0800

    产品更改

--- WeShop/Controllers/CartController.cs
+++ WeShop/Controllers/CartController.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using Shop.Model;
 using Shop.IService;
+using Senparc.Weixin.MP;
+using Senparc.Weixin;
+using Senparc.Weixin.MP.AdvancedAPIs.OAuth;
+using Senparc.Weixin.MP.Helpers;
+
 
 namespace WeShop.Controllers
 {
@@ -15,8 +20,14 @@ namespace WeShop.Controllers
         public IShopCartService shopcart { get; set; }
         public ActionResult Index()
         {
-
+            //获取用户的openid
+          string openid=  Session["oid"].ToString();
+            //var cart=shopcart.GetEntities(x=>x.Cusid==openid && )
             return View();
         }
+    }
+    public class scar
+    {
+        public int MyProperty { get; set; }
     }
 }
--- WeShop/Controllers/ProductController.cs
+++ WeShop/Controllers/ProductController.cs
@@ -15,18 +15,78 @@ namespace WeShop.Controllers
         public IProductService Productresult { get; set; }
         public ActionResult Index()
         {
-            string pcode = Request["id"];
+            string pcode = Request["id"];//从地址栏接受的值
             var product = Productresult.GetEntities(x => x.ProductSortid == pcode).ToList();
             //List<Product> product= Productresult.GetEntities(x => x.Code == pcode).ToList();
             ViewBag.product = product;
+            ViewBag.id = pcode;
+
             return View();
         }
         public ActionResult Price()
         {
+           
+            //var  product = Productresult.GetEntities(x =>true).OrderByDescending(x=>x.price).ToList();
+            //ViewBag.price = product;
+           // int id = Convert.ToInt32(Request["cid"]);//从前台页面接受的值
+                                       //List<Product> p = Productresult.GetEntities(x => true).OrderByDescending(x => x.price).ToList();
+                                       //string html = "";
+                                       //foreach (Product item in p)
+                                       //{
+                                       //    html +=@"<li>
+                                       //        <a href = '/Detail/index/?id=@item.Code' >
+                                       //              <img src = '/images/"+item.images+@"'>
+                                       //            <div class='search_item'>
+                                       //                <p>"+item.Name+@"</p>
+                                       //                <h2><i>￥</i><b>"+ item.price + @"</b></h2>
+                                       //            </div>
+                                       //        </a>
+                                       //    </li>";
+                                       //}
 
-            var  product = Productresult.GetEntities(x =>true).OrderByDescending(x=>x.price).ToList();
-            ViewBag.price = product;
-            return View();
+            
+            //测试
+            string pcode = Request["id"];
+            int id = Convert.ToInt32(Request["cid"]);
+            if (id == 0)
+            {
+                var product = Productresult.GetEntities(x => x.ProductSortid == pcode).ToList();
+                string zhtml = "";
+                foreach (var item in product)
+                {
+                    zhtml += @"<li>
+                    <a href = '/Detail/index/?id=@item.Code'>
+                          <img src = '/images/" + item.images + @"'>
+                        <div class='search_item'>
+                            <p>" + item.Name + @"</p>
+                            <h2><i>￥</i><b>" + item.price + @"</b></h2>
+                        </div>
+                    </a>
+                </li>";
+                }
+                return Content(zhtml);
+            }
+            else
+            {
+                string phtml = "";
+                //找出相对应的的价格
+                List<Product> p = Productresult.GetEntities(x => x.ProductSortid == pcode).OrderByDescending(x => x.price).ToList();
+                foreach (Product item in p)
+                {
+                    phtml += @"<li>
+                    <a href = '/Detail/index/?id=@item.Code'>
+                          <img src = '/images/" + item.images + @"'>
+                        <div class='search_item'>
+                            <p>" + item.Name + @"</p>
+                            <h2><i>￥</i><b>" + item.price + @"</b></h2>
+                        </div>
+                    </a>
+                </li>";
+                }
+                return Content(phtml);
+            }
+            //return Content(Request["cid"]);
+            //return View();
         }
     }
 }
